dc21bb14220b774b56bd2096d396bd093891e9fc
Chris <cjl104020@utdallas.edu>
2019-02-08 22:21:31 -0600

    My first commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,16 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(myDouble(2));
+    }
+    public static int myDouble(int var)
+    {
+        return var*2;
+    }
+}
